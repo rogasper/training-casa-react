696cc121bd92b967ec67fcafe6349b41d3821138
Gelar <gelargew@gmail.com>
2023-06-16 16:50:00 +0700

    deploy vercel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	i18n: {
+		locales: ['en', 'id'],
+		defaultLocale: 'en',
+	},
+	images: {
+		domains: [
+			'source.unsplash.com',
+			'picsum.photos',
+			'plus.unsplash.com',
+		],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
